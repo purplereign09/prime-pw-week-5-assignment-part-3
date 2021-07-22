5c474652cdef4ea935036b1fb7937a4fde5146a8
Patty Baraibar <pattybaraibar@msn.com>
2021-07-21 22:10:41 -0500

    made revisions to week 5 part 3

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,9 +2,6 @@ console.log('***** Music Collection *****')
 //Create a variable `collection` that starts as an empty array.
 
 
-
-
-
 //- Add a function named `addToCollection`. This function should:
   //- Take in the album's `title`, `artist`, `yearPublished` as input parameters
   //- Create a new object having the above properties
@@ -34,23 +31,18 @@ function addToCollection( title, artist, yearPublished) {
   //- Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
   //- Console.log each album as added using the returned value.
 
-console.log(' Added AlbumOne', collection.albumOne = addToCollection('Thriller', 'Michael Jackson', 1982));
-console.log(' Added AlbumTwo', collection.albumTwo = addToCollection('Back in Black', 'AC/DC', 1980));
-console.log(' Added AlbumThree', collection.albumThree = addToCollection('Bat Out of Hell', 'Meat Loaf', 1977));
-console.log(' Added AlbumFour', collection.albumFour = addToCollection('The Dark Side Of the Moon', 'Pink Floyd', 1973));
-console.log(' Added AlbumFive', collection.albumFive = addToCollection('The Bodyguard', 'Whitney Houston', 1992));
-console.log(' Added AlbumSix', collection.albumSix = addToCollection('Their Greatest Hits', 'The Eagles', 1976));
+console.log(' Added AlbumOne', addToCollection('Thriller', 'Michael Jackson', 1982));
+console.log(' Added AlbumTwo', addToCollection('Back in Black', 'AC/DC', 1980));
+console.log(' Added AlbumThree', addToCollection('Bat Out of Hell', 'Meat Loaf', 1977));
+console.log(' Added AlbumFour', addToCollection('The Dark Side Of the Moon', 'Pink Floyd', 1973));
+console.log(' Added AlbumFive', addToCollection('The Bodyguard', 'Whitney Houston', 1992));
+console.log(' Added AlbumSix', addToCollection('Their Greatest Hits', 'The Eagles', 1976));
 
 //- After all are added, console.log the `collection` array.
 console.log('Collection', collection);
 
+//This function has been corrected and the unnecessary object property has been deleted.
 
-
-//- Add a function named `showCollection`. This function should:
-  //- Take in an array parameter. (This allows it to be reused to show any collection,
-  // --like the results from the find or search.)
-  //- Console.log the number of items in the array.
-  //- Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
 
 
 function showCollection(results){
@@ -69,30 +61,32 @@ function showCollection(results){
     //- Loop through the `collection` and add any objects with a matching artist to the array.
     //- Return the array with the matching results. If no results are found, return an empty array.
 
-  //- Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
+  //- Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection.
+  //Check that for artists with multiple matches, all are found.
 
-function findByArtist(artist){
+function findByArtist(artist) {
   let results = [];
-  //create an array to hold results as an empty array first
-  for(const item of collection){
-    if(item.artist === artist){
-    results.push(item.artist)
-    return results = [item.artist];
-    }
-  //looping through collection
-
-  //adding objects with item.artist that have been looped through AND results of 'artist ' string parameter
+  for(let i = 0; i < collection.length; i++){
+    if(artist === collection[i].artist){
+      results.push(collection[i].artist);
     }
   }
+  return results;
+}
 
+  //collection.artist = "Pink Floyd";
   console.log(findByArtist('Pink Floyd'));
+  console.log(findByArtist('Whitney Houston'));
+  console.log(findByArtist('DMX'));
+//This code has been corrected to reflect multiple search results, including an artist in my playlist and an artist and an artist not in it.
+
+//Stretch Goal
 
 
-//Stetch Goal
-
-
-function search( value = {}){
+function search(value){
+  //taking in an input parameter
   if(Object.keys(value).length === 0){
+    //checking if the object has a value
     return collection
   }
   let song = [];
@@ -108,24 +102,8 @@ function search( value = {}){
   //return the collection's artists and the year published
 }
 
-
-  const jazz = {
-  artist: 'Michael Jackson',
-  year: 1982
-}
-
-const empty = {
-
-}
-
-console.log(search());
-  //- Create a function called `search`. This function should:
-    //- Take an input parameter for a search criteria object.
-    //-Create your solution based on a search object that has these properties:
-
-     //artist: 'Ray Charles', year: 1957 }
-
-     //- The returned output from `search` should meet these requirements:
-      // - Return a new array of all items in the `collection` matching *all* of the search criteria.
-       //- If no results are found, return an empty array.
-       //- If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
+let myArtist = { artist: 'Michael Jackson', year: 1982 };
+console.log(search({ artist: 'Ray Charles', year: 1957 }));//not in music
+console.log(search({ artist: 'Michael Jackson', year: 1982 }));
+console.log(search(myArtist));
+//This function now takes in multiple parameters, including an artist that isn't there, and one that is there
